1bc4ae705585aa25c7a4f3efaa7e58477f1c0cf3
Joyce Kim <joyce@knewton.com>
2013-07-13 19:03:52 -0400

    test

---
+++ src/main/java/com/animalcompany/animalfarm/maintest.java
@@ -0,0 +1,11 @@
+package com.animalcompany.animalfarm;
+
+import com.google.common.collect.ImmutableList;
+
+public class maintest {
+    ImmutableList<String> strings;
+
+    public static void main(String [] args) {
+        System.out.println("testing");
+    }
+}
